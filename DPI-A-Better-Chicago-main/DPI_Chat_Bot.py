51ff0f2c8d70015a4d1961b8bb9eecf9af4d3d1e
KadijaSylla <ksyll2@uic.edu>
2024-07-30 11:30:29 -0500

    adding changes to the chat bot

--- DPI-A-Better-Chicago-main/DPI_Chat_Bot.py
+++ DPI-A-Better-Chicago-main/DPI_Chat_Bot.py
@@ -40,58 +40,19 @@ chat_session = model.start_chat(
   history=[
     {
         'role': 'user',
-        'parts': ['You are a specialist at determining grant eligibility for A-Better-Chicago, a project hosted by DPI.']
+        'parts': ['You are a specialist at determining grant eligibility for A-Better-Chicago, a project hosted by DPI. Give an answer to give grants based on education, underrepresented communitites, low income communities in chicago.']
     },
     {
         'role': 'model',
-        'parts': ['Sure.'],
+        'parts': ['Sure. Let me get an answer for you'],
     },
   ]
 )
 
-
-# resp = chat_session.send_message(
-#     content='Does McDonalds quality for a grant according to the project a-better-chicago',
-# )
-
-# p
-# rint(resp.text)
-
-response = model.generate_content([
-  "input: McDonald's",
-  "output: This is not aligned with A-Better-Chicago's values. Please provide a non-profit organization. This is not aligned because they do not support health and wellness initiatives",
-  "input: Burger King",
-  "output: This is not aligned with A-Better-Chicago's values. Please provide a non-profit organization. This is not aligned because they do not support health and wellness initiaitves",
-  "input: Wendy's",
-  "output: This is not aligned with A-Better-Chicago's values. Please provide a non-profit organization. This is not aligned because they do not support health and wellness initiaitves",
-  "input: Culvers",
-  "output: This is not aligned with A-Better-Chicago's values. Please provide a non-profit organization. This is not aligned because they do not support health and wellness initiaitves",
-  "input: In and Out",
-  "output: This aligns with A-Better-Chicago values! This organization helps with providing students.",
-  "input: Dominos",
-  "output: This aligns with A-Better-Chicago values! This company donates to the partner's foundation which helps employees facing hardships like natural disasters or injuries.",
-  "input: A House in Austin",
-  "output: This organization aligns with A-Better-Chicago values!",
-  "input: Dream on Education",
-  "output: This organization aligns with A-Better-Chicago values!",
-  "input: Digital Scholars",
-  "output: This organization aligns with A-Better-Chicago values!",
-  "input: Everyone Can Code",
-  "output: This organization aligns with A-Better-Chicago values!",
-  "input: Chicago Lighthouse Mission and Activities",
-  "output: This organization is a 990 organization that aligns with A-Better-Chicago values because its mission is to provide education and youth development, provides vocational and employment services, and enable comprehensive support services",
-  "input: Carole Robertson Center",
-  "output: This organization is a 990 organization and aligns with A-Better-Chicago values because it provides education, family and community support, and provides high-impact programs",
-  "input: Chicago HOPES for Kids",
-  "output: This organization is a 990 organization and aligns with A-Better-Chicago values because it focuses on education, supports vulnerable populations, and has a community impact",
-  "input: College Board",
-  "output: This organization is a non-profit organization but does not align with A-Better-Chicago's values because it does not align with opportunities for low-income Black and Latinx Youth nor support Chicago’s high-impact nonprofits.",
-])
-
-
-
 def getGeminiResponse(question):
-    response = model.generate_content(question)
+    response = chat_session.send_message(
+      content = question
+    )
     return response.text
 
 st.set_page_config(page_title="A Better Chicago")
@@ -105,7 +66,7 @@ if submit:
     response = getGeminiResponse(input)
     st.subheader("This is what we think...")
     st.write(response)
-else:
-    response = getGeminiResponse("Give a response that states to the user to give a company that they think could qualify for a grant")
-    st.subheader("Give a Company That Could Qualify for a Grant")
-    st.write(response)
+# else:
+#     response = getGeminiResponse("Give a response that states to the user to give a company that they think could qualify for a grant")
+#     st.subheader("Give a Company That Could Qualify for a Grant")
+#     st.write(response)
